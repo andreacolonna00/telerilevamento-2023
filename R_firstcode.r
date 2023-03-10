37399ddc1851c83d922852a87cc42dccd684a3ba
andreacolonna00 <127427421+andreacolonna00@users.noreply.github.com>
2023-03-10 17:45:11 +0100

    Update R_firstcode.r

--- R_firstcode.r
+++ R_firstcode.r
@@ -4,3 +4,17 @@
 install.packages("raster")
 
 library(raster)
+
+# import dei dati, setto la cartella di lavoro
+
+setwd("C:/lab/") # windows
+# studiamo cosa avviene nella riserva di Paracana
+# path 224, row 63 (posizione)
+
+l2011 <- brick("p224r63_2011_masked.grd")
+
+# plotto i dati
+plot(l2011)
+
+cl <- colorRampPalette(c("red","orange","yellow")) (100) # 100 sono le sfumature
+plot(l2011[[4]], col=cl) # banda infrarosso
