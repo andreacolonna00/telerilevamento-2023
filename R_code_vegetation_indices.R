82426187b07aa5dbdf04054e3735a9cdf72099be
andreacolonna00 <127427421+andreacolonna00@users.noreply.github.com>
2023-03-30 17:28:05 +0200

    Update R_code_vegetation_indices.R

--- R_code_vegetation_indices.R
+++ R_code_vegetation_indices.R
@@ -23,4 +23,31 @@ dvi1992 <- l1992[[1]]-l1992[[2]]
 cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100)
 plot(dvi1992, col=cl) # nelle zone rosse il DVI è alto
 
+l2006 <- brick("defor2_.png")
+l2006
+plotRGB(l2006, 1,2,3, stretch="lin")
+
+# Esercizio: plotto 1992 sopra 2006
+par(mfrow=c(2,1))
+plotRGB(l1992, 1,2,3, stretch="lin")
+plotRGB(l2006, 1,2,3, stretch="lin")
+dev.off()
+dvi2006 <- l2006[[1]]-l2006[[2]]
+plot(dvi2006, col=cl)
+
+# ANALISI MULTITEMPORALE
+difdvi = dvi1992 - dvi2006
+cld <- colorRampPalette(c('blue','white','red'))(100) # per daltonici
+plot(difdvi, col=cld)
+
+# NDVI 1992
+ndvi1992 = dvi1992 / (l1992[[1]]+l1992[[2]])
+plot(ndvi1992, col=cl)
+
+# NDVI 2006
+ndvi2006 = dvi2006 / (l2006[[1]]+l2006[[2]])
+plot(ndvi2006, col=cl)
+
+difndvi = ndvi1992 - ndvi2006
+plot(difndvi, col=cld)
 
